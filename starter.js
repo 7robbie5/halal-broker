e1c5a69ae9974654b475746bef4c1dfe61269560
7robbie5 <70966215+7robbie5@users.noreply.github.com>
2021-06-01 15:40:58 +0200

    Add files via upload

--- starter.js
+++ starter.js
@@ -18,5 +18,5 @@
 // If you're importing tooltips or popovers, be sure to include our Popper.js dependency
 // import "../../node_modules/popper.js/dist/popper.min.js";
 
-import "../../node_modules/bootstrap/js/dist/util.js";
-import "../../node_modules/bootstrap/js/dist/modal.js";
+import "util.js";
+import "modal.js";
