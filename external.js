79c81a33b5d8bafe051825f151f7defccfe82057
7robbie5 <70966215+7robbie5@users.noreply.github.com>
2021-06-02 13:44:35 +0200

    Update external.js

--- external.js
+++ external.js
@@ -2,22 +2,14 @@
   * External Links external.js v0.0.1 (https://www.robhonig.com)
   * Copyright 2021 7robbie5 (https://7robbie5.github.io/)
   */
-
-$('.ref a').external({
-
-  // (string) name of the external window
-  name:     "_blank", 
-
-  // (bool) use the "target" HTML attribute to support Firefox tab handling and Safari status bar info
-  target:   true, 
-
-  // (string) apply CSS classes to an external link
-  //  classes:  "", 
-
-  // (string) apply a relationship to an external link
-  relation: "external", 
-
-  // (string) text added as or appended to the "title" attribute to inform the user about an upcoming new window
-  //  title:    "Opens in a new window" 
-
-});
+function externalLinks() {
+ if (!document.getElementsByTagName) return;
+ var anchors = document.getElementsByTagName("a");
+ for (var i=0; i<anchors.length; i++) {
+   var anchor = anchors[i];
+   if (anchor.getAttribute("href") &&
+       anchor.getAttribute("rel") == "external")
+     anchor.target = "_blank";
+ }
+}
+window.onload = externalLinks;
